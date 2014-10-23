4440c412138f5c5acfbb3f4a2558a51cee57a5f1
hzz <hzz.gofur@gmail.com>
2014-10-23 14:27:10 +0800

    删除了一些多余文件

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141015020837) do
+ActiveRecord::Schema.define(:version => 20141023060520) do
 
   create_table "refinery_blog_categories", :force => true do |t|
     t.string   "title"
