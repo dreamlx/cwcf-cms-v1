e20623f95fd1f9a725220561ea1b4b70c0afd8c3
hzz <hzz.gofur@gmail.com>
2014-11-22 18:56:11 +0800

    倒计时

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -382,4 +382,36 @@ $(function () {
         $(".photo-view").html(this.outerHTML);
       });
     }
+
+    if ($("#myDate").length > 0){
+      var $input = $("#myDate");
+      var time_div = $input.parent().parent();
+      var deadline = $input.val();
+
+      function timeElapse(date){
+        var current = Date();
+        var seconds = (Date.parse(date) - Date.parse(current)) / 1000;
+        var days = Math.floor(seconds / (3600 * 24));
+        seconds = seconds % (3600 * 24);
+        var hours = Math.floor(seconds / 3600);
+        if (hours < 10) {
+          hours = "0" + hours;
+        }
+        seconds = seconds % 3600;
+        var minutes = Math.floor(seconds / 60);
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+        seconds = seconds % 60;
+        if (seconds < 10) {
+          seconds = "0" + seconds;
+        }
+        var result = "<span class=\"digit\">" + days + "</span> <span>天</span> <span class=\"digit\">" + hours + "</span> <span>小时</span> <span class=\"digit\">" + minutes + "</span> <span>分</span> <span class=\"digit\">" + seconds + "</span> <span>秒</span>";
+        time_div.html(result);
+      }
+      timeElapse(deadline);
+      setInterval(function () {
+        timeElapse(deadline);
+      }, 500);
+    }
 });
