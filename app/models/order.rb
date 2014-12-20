09ec3dba958626702bed81896f214170806a8796
ZengWeimao <so.zengtao@gmail.com>
2014-12-20 12:51:38 +0800

    order status

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  before_create :set_default_status
+
   has_many :line_items, dependent: :destroy
 
   def add_line_items_from_cart(cart)
@@ -7,5 +10,15 @@ class Order < ActiveRecord::Base
       item.cart_id = nil
       line_items << item
     end
+  end
+
+  def get_user
+    Refinery::User.find(self.user_id)
+  end
+
+  private
+
+  def set_default_status
+    self.status = "applied"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141220013216) do
+ActiveRecord::Schema.define(:version => 20141220044722) do
 
   create_table "blanks", :force => true do |t|
     t.string   "co_name"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(:version => 20141220013216) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+    t.string   "status"
   end
 
   create_table "positions", :force => true do |t|
